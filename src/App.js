21f1f8c880adeef21d5c19f014bb357f6014ce2e
mbmc7444 <mbmc7444@gmail.com>
2021-08-30 02:33:59 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
     movies: []
   };
   getMovies = async () => {
-    const { data: { data: { movies } } } = await axios.get("http://yts-proxy.now.sh/list_movies.json");
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
     this.setState({ movies, isLoading: false })
   };
   componentDidMount() {
